4bb604fdbc78455b411ad12162f1f9dae6059b55
Postaron <florent.denef@gmail.com>
2017-11-09 14:34:04 +0100

    outilsListe : adapté au bon type de donnée. graphe.c : fini la création des listes adjacences mais non testé

--- graphe.c
+++ graphe.c
@@ -1,10 +1,13 @@
-#include "graphe.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "graphe.h"
+#include "cellule.h"
+#include "outilsListe.h"
 
 void creerListesAdjacences(graphe_t * grapĥ) {
 	FILE * file = NULL;
+	int indice = 0, donnee = 0;
 	char buffer[27]; // buffer suffisamment grand
 	file = fopen("graphe1.txt", "r");
 	if (file == NULL) {
@@ -12,8 +15,7 @@ void creerListesAdjacences(graphe_t * grapĥ) {
 		exit(EXIT_FAILURE);
 	}
 
-	while (!feof(file)) {
-		fscanf(file, "%s", buffer);
+	while (fscanf(file, "%s", buffer), !feof(file)) {
 		if (strcmp(buffer, "nSommets") == 0) {
 			fscanf(file, "%d", grapĥ->nSommets);
 			grapĥ->adj = (liste_t **) malloc(
@@ -29,15 +31,30 @@ void creerListesAdjacences(graphe_t * grapĥ) {
 		} else if (strcmp(buffer, "complet") == 0) {
 			fscanf(file, "%d", grapĥ->complet);
 		} else if (strcmp(buffer, "debutDefAretes") == 0) {
-			while (strcmp(buffer, "finDefAretes") != 0) {
-				//TODO finir ça
+			while (fscanf(file, "%s", buffer), strcmp(buffer, "finDefAretes")
+					!= 0) { //Nouvelle condition
+				/*
+				 * Non testé
+				 * Bon avec nouvelle condition ?
+				 */
+				indice = atoi(buffer);
+				fscanf(file, "%s", buffer);
+				donnee = atoi(buffer);
+				inserer(grapĥ->adj[indice][0], initialiserCellule(donnee));
+				if (grapĥ->oriente) {
+					inserer(grapĥ->adj[donnee][0], initialiserCellule(indice));
+				}
 			}
 		}
 	}
+	fclose(file); //Normalement c'est bon cette ligne
 }
 
 void afficherListesAdjacences(graphe_t * graph) {
-
+	int i;
+	for (i = 0; i < graph->nSommets; ++i) {
+		afficherListe(graph->adj[i]);
+	}
 }
 
 void creerMatriceAdjacences(graphe_t * grapĥ) {
--- outilsListe.c
+++ outilsListe.c
@@ -6,7 +6,7 @@ void afficherListe(liste_t * pliste) {
 	cellule_t * ptemp;
 	ptemp = pliste->tete;
 	do {
-		puts(ptemp->mot);
+		puts(ptemp->value);
 		if (ptemp->succ != NULL) {
 			ptemp = ptemp->succ;
 		} else {
